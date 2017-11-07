8b21ee97569b1629352aebc023d3b286f0cb8649
taira120 <taira1999120@gmail.com>
2017-11-07 18:26:37 +0900

    ヘッダー修正

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,30 +1,38 @@
 <template>
-  <v-container
-    fluid
-    style="min-height: 0;"
-    grid-list-lg
-    >
-    <v-layout row wrap>
-      <v-flex xs12 v-for="i in 3" :key='i'>
-        <v-card color="blue-grey darken-2" class="white--text">
-          <v-card-title primary-title>
-            <div class="headline">Unlimited music now</div>
-            <div>Listen to your favorite artists and albums whenenver and wherever, online and offline.</div>
-          </v-card-title>
-          <v-card-media
-                 :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
-                 height="150px"
-                 >
-          </v-card-media>
-            <v-card-actions>
-              <v-btn flat dark>
-                <v-icon>fa-search</v-icon>
-              </v-btn>
-            </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-toolbar app scroll-off-screen>
+      <v-btn icon>
+        <v-icon>fa-search</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>fa-plus</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-container
+        fluid
+        style="min-height: 0;"
+        grid-list-lg
+        >
+        <v-layout row wrap>
+          <v-flex xs12 v-for="i in 3" :key='i'>
+            <v-card color="blue-grey darken-2" class="white--text">
+              <v-card-title primary-title>
+                <div class="headline">Unlimited music now</div>
+                <div>Listen to your favorite artists and albums whenenver and wherever, online and offline.</div>
+              </v-card-title>
+              <v-card-media
+                     :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
+                     height="150px"
+                     >
+              </v-card-media>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </main>
+  </div>
 </template>
 
 <script>
