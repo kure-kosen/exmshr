124cf325f51e4d97f3c2af0a9ee2a1d54f1b13ad
hasikawa <hasikawamaru@gmail.com>
2017-11-09 14:29:48 +0900

    ボタンを一つ配置した

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -1,1 +1,42 @@
-aaaa
+
+<template>
+  <div>
+    <v-toolbar app scroll-off-screen>
+      <v-btn icon>
+        <v-icon>fa-search</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>fa-plus</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-container
+        fluid
+        style="min-height: 0;"
+        grid-list-lg
+        >
+      <v-btn light disabled>ボタン</v-btn>
+　　　
+      </v-container>
+   </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true
+  }),
+  props: {
+    source: String
+  }
+}
+</script>
+
+<style lang="stylus">
+.group
+    display: flex
+    flex: 1
+    justify-content: space-around
+</style>
