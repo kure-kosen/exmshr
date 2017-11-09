49ae36cb432ef123c0d9064a1dc18aaa3b6c97ad
taira120 <taira1999120@gmail.com>
2017-11-09 14:29:10 +0900

    ボタンを１個置いた

---
+++ src/pages/post.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div>
+    <v-toolbar app scroll-off-screen>
+      <v-btn icon>
+        <v-icon>fa-search</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>fa-plus</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main>
+      <v-container
+        fluid
+        style="min-height: 0;"
+        grid-list-lg
+        >
+         <v-btn fab dark small color="primary">
+           <v-icon dark>fa-remove</v-icon>
+         </v-btn>
+      </v-container>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true
+  }),
+  props: {
+    source: String
+  }
+}
+</script>
+
+<style lang="stylus">
+.group
+    display: flex
+    flex: 1
+    justify-content: space-around
+</style>
