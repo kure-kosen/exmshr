9fef77ed89932c45e3e090f5d927f4e52b3287bd
hasikawa <hasikawamaru@gmail.com>
2017-11-16 14:45:44 +0900

    コンポーネント配置

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -20,12 +20,13 @@
 	<v-select v-bind:items="dropdown_font1" overflow label="教科" append-icon="fa-arrow-down"></v-select>
         <div>学年</div>
         <v-select v-bind:items="dropdown_font2" overflow label="学年" append-icon="fa-arrow-down"></v-select>
+        <div>期間</div>
+        <v-select v-bind:items="dropdown_font3" overflow label="期間" append-icon="fa-arrow-down"></v-select>
+
         <v-layout row wrap>
         <v-flex xs3 v-for="i in 3" :key="i">
-        <v-card dark color="secondary">
-          <v-card-text class="px-0"></v-card-text>
-        </v-card>
-      </v-flex> 
+      
+        </v-flex> 
          <v-btn color="search">検索</v-btn>
         </v-layout>
              
@@ -40,6 +41,8 @@ export default {
     drawer: true,
     dropdown_font1: ['bando', '坂東', '微積', '物理'],
     dropdown_font2: ['１年','２年','３年','4年','5年'],
+    dropdown_font3: ['前期中間','前期期末','後期中間','学年末'],
+
   }),
   props: {
     source: String
