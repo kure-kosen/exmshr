44b0d168f84c9ad0c440e7536565782d67a423a6
hasikawa <hasikawamaru@gmail.com>
2017-11-16 14:25:03 +0900

    コンポーネントの配置

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -16,8 +16,19 @@
         style="min-height: 0;"
         grid-list-lg
         >
-      <v-btn light disabled>ボタン</v-btn>
-　　　
+        <div>教科</div>
+	<v-select v-bind:items="dropdown_font1" overflow label="教科" append-icon="fa-arrow-down"></v-select>
+        <div>学年</div>
+        <v-select v-bind:items="dropdown_font2" overflow label="学年" append-icon="fa-arrow-down"></v-select>
+        <v-layout row wrap>
+        <v-flex xs3 v-for="i in 3" :key="i">
+        <v-card dark color="secondary">
+          <v-card-text class="px-0"></v-card-text>
+        </v-card>
+      </v-flex> 
+         <v-btn color="search">検索</v-btn>
+        </v-layout>
+             
       </v-container>
    </main>
   </div>
@@ -26,7 +37,9 @@
 <script>
 export default {
   data: () => ({
-    drawer: true
+    drawer: true,
+    dropdown_font1: ['bando', '坂東', '微積', '物理'],
+    dropdown_font2: ['１年','２年','３年','4年','5年'],
   }),
   props: {
     source: String
